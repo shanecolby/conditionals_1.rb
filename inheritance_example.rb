e0bc05feaf79b1a0208f4f15ee6720ae6bd5d443
shane <shanecolbyzero8@gmail.com>
2020-10-28 20:45:57 -0500

    car bike class solution

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,5 @@
-class Car
+
+class Vehicle
   def initialize
     @speed = 0
     @direction = 'north'
@@ -15,34 +16,22 @@ class Car
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
+class Car < Vehicle
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike <  Car
-
-  # def initialize
-  #   @speed = 0
-  #   @direction = 'north'
-  # end
-
-  # def brake
-  #   @speed = 0
-  # end
-
-  # def accelerate
-  #   @speed += 10
-  # end
-
-  # def turn(new_direction)
-  #   @direction = new_direction
-  # end
-
+class Bike < Vehicle
   def ring_bell
     puts "Ring ring!"
   end
 end
 
+bike = Bike.new
+car = Car.new
 
+bike.ring_bell
+car.honk_horn
