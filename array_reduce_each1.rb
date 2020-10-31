9c58a1c59dcd96f139481b6cfa5a0df16b4508ef
shane <shanecolbyzero8@gmail.com>
2020-10-31 10:49:20 -0500

    redcing a hash within an array with each method

---
+++ array_reduce_each1.rb
@@ -0,0 +1,8 @@
+items = [{name: "Book", price: 45}, {name: "cup", price: 10}, {name: "computer", price: 1500}, {name: "car", price: 45000}, {name: "brithday cake", price: 30}, {name: "playstation5", price: 500}]
+items_with_short_names = []
+items.each do |item|
+  if item[:name].length < 5
+    items_with_short_names << item
+  end
+end
+p items_with_short_names
