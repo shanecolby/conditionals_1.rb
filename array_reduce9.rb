1a48814d2bc7d02f61992e73a0a5e18c0afc6efe
shane <shanecolbyzero8@gmail.com>
2020-10-31 09:20:21 -0500

    reducing array of hases to find item with the shortest name

---
+++ array_reduce9.rb
@@ -0,0 +1,10 @@
+items = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
+i = 0
+shorest_item_name = items[0]
+while i < items.length
+  if items[i][:name].length < shorest_item_name[:name].length
+    shorest_item_name = items[i]
+  end
+  i += 1
+end
+p shorest_item_name
