25db0ad89e046b4931df2fa8788e0740e86f5947
shane <shanecolbyzero8@gmail.com>
2020-10-31 09:27:45 -0500

    reducing array of integer to find hightest or max number

---
+++ array_reduce10.rb
@@ -0,0 +1,10 @@
+numbers = [21,27,25,29,43,24,17,43,25,39,54,34,20]
+i = 0
+max_number = numbers[0]
+while i < numbers.length
+  if numbers[i] > max_number
+    max_number = numbers[i]
+  end
+  i += 1
+end
+p max_number
