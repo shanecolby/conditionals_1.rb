8dfd5d0b1f84fdfa467c39f52f953ca6c72b2be9
shane <shanecolbyzero8@gmail.com>
2020-11-21 15:59:42 -0600

    turned array into hased pairs

---
+++ HashToArray.rb
@@ -0,0 +1,10 @@
+numbers = [[1, 3], [8, 9], [2, 16]]
+number_pair_hash = {}
+index = 0
+while index < numbers.length
+  key = numbers[index][0]
+  value = numbers[index][1]
+  number_pair_hash[key] = value
+  index += 1
+end
+p number_pair_hash
