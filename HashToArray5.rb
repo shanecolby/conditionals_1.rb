f2a241c192ff4d903b8868449ea69a013d463327
shane <shanecolbyzero8@gmail.com>
2020-11-21 16:16:27 -0600

    hash into array of hashes

---
+++ HashToArray5.rb
@@ -0,0 +1,7 @@
+people = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
+people_array = []
+people.each do |id, person|
+  person[:id] = id
+  people_array << person
+end
+p people_array
