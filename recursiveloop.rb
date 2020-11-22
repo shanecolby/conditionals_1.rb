6f10db4690b0dac4833a913a277319f4810eece0
shane <shanecolbyzero8@gmail.com>
2020-11-22 16:31:32 -0600

    factorial recursion loop

--- recursiveloop.rb
+++ recursiveloop.rb
@@ -1,10 +1,22 @@
+# def recursive_loop(number)
+#   if number == 0
+#     return number
+#   else
+#     recursive_loop(number - 1)
+#     p number
+#   end
+
+# end
+# recursive_loop(100)
+
+# 5 * 4 * 3 * 2 * 1 =
+
 def recursive_loop(number)
-  if number == 0
-    return number
+  if number == 1
+    return 1
   else
-    recursive_loop(number - 1)
-    p number
+    return (number * recursive_loop(number - 1))
   end
 
 end
-recursive_loop(100)
+p recursive_loop(5)
