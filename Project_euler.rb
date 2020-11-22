0bbad0480aa42076f9c08850977499e315af3553
shane <shanecolbyzero8@gmail.com>
2020-11-22 15:59:59 -0600

    project euler #6

---
+++ Project_euler.rb
@@ -0,0 +1,19 @@
+number = 1
+sum_of_squares = 0
+
+number = 1
+10.times do 
+  sum_of_squares += number ** 2
+  number += 1
+end
+p sum_of_squares
+
+
+
+number = 1
+square_of_sum = 0
+10.times do 
+  square_of_sum += number
+  number += 1
+end
+p square_of_sum ** 2 - sum_of_squares
