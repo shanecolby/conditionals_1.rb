855df1bdb30cd8578b221f91c1a15862f8c64b04
shane <shanecolbyzero8@gmail.com>
2021-01-21 06:41:10 -0600

    selecting items from an array and moving to new array-whiteboard

---
+++ less_than_100.rb
@@ -0,0 +1,14 @@
+def less_than_100(array)
+  index = 0
+  new_array = []
+  
+  while index < array.length
+    if array[index] < 100
+      new_array << array[index]
+    end
+    index += 1
+  end
+  return new_array
+
+end
+p less_than_100([99,101,88,4,25,50,2000])
