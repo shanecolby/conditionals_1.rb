28d8c3271a038e0f37ac40d3186d0aac367b29a9
shane <shanecolbyzero8@gmail.com>
2021-02-10 07:07:42 -0600

    every other element in array index

--- every_other.rb
+++ every_other.rb
@@ -10,3 +10,17 @@ def every_other(array)
 
 end
 p every_other([99,101,88,4,25,50,2000])
+
+def everyOther(array)
+  index = 0
+  new_array = []
+  while index < array.length
+    new_array << array[index]
+    index += 2
+  end
+  return new_array
+
+
+
+end
+p everyOther([1,2,3,4,5,6,7,8,9])
