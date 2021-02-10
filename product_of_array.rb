adbef1a7352c23dd0cfe7fd6f124164a94ca6be8
shane <shanecolbyzero8@gmail.com>
2021-02-10 07:14:53 -0600

    refactor priactice problems

--- product_of_array.rb
+++ product_of_array.rb
@@ -10,3 +10,15 @@ def product(array)
 end
  
 p product([1,2,3,4,5])
+
+def product(array)
+  index = 0
+  product = array[index]
+  while index < array.length
+    product = product * array[index]
+    index += 1
+  end
+  return product
+end
+
+p product([1,2,3,4,5])
--- reverse_array.rb
+++ reverse_array.rb
@@ -9,3 +9,15 @@ def reverse(array)
 end
  
 p reverse([1,2,3,4,5])
+
+def reverse(array)
+  index = 0
+  number = array[index]
+  while index < array.length
+    
+    index += 1
+  end
+  return array.reverse
+
+end
+p reverse([1,2,3,4,5])
