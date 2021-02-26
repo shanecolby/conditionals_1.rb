e1d7d0fbb3390f0c73860ccdb6b17b0cde603353
shane <shanecolbyzero8@gmail.com>
2021-02-26 06:57:50 -0600

    refactor

--- HashToArray2.rb
+++ HashToArray2.rb
@@ -6,3 +6,13 @@ while index < items.length
   index += 1
 end
 p items_hash
+
+
+items = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+items_hash = {}
+index = 0
+while index < items.length
+  items_hash[items[index][:id]] = items[index]
+  index += 1
+end
+p items_hash
